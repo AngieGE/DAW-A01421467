01199ec21645d66b7ea4259f0254f42c11709e4d
AngieGE <30639215+AngieGE@users.noreply.github.com>
2018-01-21 17:21:32 -0600

    terminé entrada 1

--- Labo4/js/index.js
+++ Labo4/js/index.js
@@ -1,19 +1,52 @@
-var texto= prompt("Ingresa un número");
+tabla();
+/*suma("2");
+contador("3");
+promedios("4");
+inversa("5");
+problema("6");*/
 
-var res= new Array(texto);
-for (var i = 0; i <texto; i++) {
-	res[i]=i+1;
+function tabla(){  					/*ENTRADA 1*/
+	var texto = prompt("Ingresa un número");
+	var res= new Array(texto);
+	for (var x = 1; x < 4; x++) {
+		for (var y = 0; y < texto; y++) {
+			if (x==1) {
+				var identificador = document.getElementById("numerados");
+				res[y]=y+1;
+			}else if(x==2){
+				var identificador = document.getElementById("cuadrado");
+				res[y]= Math.pow(y+1, 2);
+			}else{
+				var identificador = document.getElementById("cubo");
+				res[y]= Math.pow(y+1, 3);
+			}
+		}
+		identificador.innerHTML = res;
+	}
 }
 
-var primera;
-primera = document.getElementById('1');
+function suma(pIdentificador){
+	var Identificador=pIdentificador;
+	var a = Math.floor((Math.random() * 100) + 1);
+	var b = Math.floor((Math.random() * 100) + 1);
+	texto = prompt("Suma " + a + " + "+b);
+	var suma = a + b;
+	if(texto==suma){
+		res = 'Correcto. La suma de '+ a + ' + '+ b + ' = '+ suma;
+	}else{
+		res = 'Incorrecto. La suma de '+ a + ' + '+ b + ' = '+ suma;
+	}
+	var pIdentificador = document.getElementById(pIdentificador);
+	Identificador.textContent= res;
+}
 
-primera.textContent= res;
-
+function contador(pIdentificador){}
+function promedios(pIdentificador){}
+function inversa(pIdentificador){}
+function problema(pIdentificador){}
 
 /*
 ESTOY EN DEVELOP
-
 <scrypt type
 
 Es un lenguaje no tipado. (Podemos estar trabajando con un dato y despues decirle si es int, float, string..)
