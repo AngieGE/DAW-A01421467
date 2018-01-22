10f8a87eeee94b4361a7558fec164dc985026499
AngieGE <30639215+AngieGE@users.noreply.github.com>
2018-01-21 22:42:24 -0600

    lab 4 terminado

--- Labo4/js/index.js
+++ Labo4/js/index.js
@@ -1,11 +1,6 @@
-//tabla();
-//suma();sionó
-//contador();
-//promedios();
-inversa();
 //problema();
 
-function tabla(){  					/*ENTRADA 1*/
+function verTabla(){  				/*ENTRADA 1 TERMINADA*/
 	var texto = prompt("Ingresa un número");
 	var res= new Array(texto);
 	for (var x = 1; x < 4; x++) {
@@ -25,7 +20,7 @@ function tabla(){  					/*ENTRADA 1*/
 	}
 }
 
-function suma(){
+function verSuma(){					/*ENTRADA 2 TERMINADA*/
 	var Identificador= document.getElementById('2');
 	var a = Math.floor((Math.random() * 100) + 1);
 	var b = Math.floor((Math.random() * 100) + 1);
@@ -39,29 +34,26 @@ function suma(){
 	Identificador.innerHTML = res;
 }
 
-function contador(){
-	console.log("entro a contador");
-	var Identificador= document.getElementById('3');
+function verContador(){				/*FUNCION 1 TERMINADA*/
+	var Identificador= document.getElementById('3'); //Las cajitas
 	var texto= '<br>';
-	document.getElementById('btn1').onclick = function(){
-		console.log("presionó submit");
-		var max = document.getElementById('tamanoArreglo').value;
-		console.log("valor max: "+ max);
-		for (var i = 0; i < max; i++) { //Que se escrivan max inputs para que ingrese los numeros
+	var max = document.getElementById('tamanoArreglo').value;
+	for (var i = 0; i < max; i++) { //Que se escrivan max inputs para que ingrese los numeros
 		Identificador.innerHTML = Identificador.innerHTML.concat('<input type="number" name="quantity " ' + 'id="numeros' +i+'"> <br>');
 		console.log(Identificador.innerHTML);
-		}
-		
 	}
 	document.getElementById('btn2').onclick = function(){
 		console.log("presionó ver Resultados");
 		var max = document.getElementById('tamanoArreglo').value;
-		console.log("valor max: "+ max);
+		var sMax= max.value;
+		console.log("valor max: "+ sMax);
 		var neg=0, pos=0, cero=0;
 		for (var i = 0; i < max; i++) { //Que se escrivan max inputs para que ingrese los numeros
-			if (document.getElementById('numeros'+i) < 0) {
+			var valor = document.getElementById('numeros'+i).value;
+			console.log("valor: " + valor);
+			if ( valor < 0) {
 				neg=neg+1;
-			}else if(document.getElementById('numeros'+i) > 0){
+			}else if(valor > 0){
 				pos=pos+1;
 			}else{
 				cero=cero+1;
@@ -78,19 +70,54 @@ function contador(){
 	}
 }
 
-function promedios(){
-
-}
-
-function inversa(){
-	var numero = document.getElementById('numerof3');
-	for (var i = 0; i < range.length; i++) {
-	var n = range[i];   
+function VerPromedios(){			/*FUNCION 1 TERMINADA*/
+	var res;
+	var promedio = 0;
+	var cal;
+	for (var i = 0; i < 3; i++) {
+		for (var x = 0; x < 3; x++) {
+			cal = document.getElementById('a'+(i+1)+'c'+(x+1)).value;
+			promedio = promedio + +cal;
+		}
+		promedio=promedio/3;
+		res = document.getElementById('a'+(i+1)+'p');
+		res.innerHTML = promedio;
+		promedio = 0;
 	}
 }
 
-function problema(){
+function verInversa(){  				/*FUNCION 3 TERMINADA*/
+	var number = document.getElementById('numerof3').value;
+	console.log('number: '+ number);
+	var sNumber = number.toString();
+	var res = '';
+	console.log('longitud: '+ sNumber.length);
+	for (var i = number.length-1; i >=0; i--) {
+		console.log('number en i: '+number[i]);
+		res= res.concat(number[i]); 
+	}
+	console.log(res);	
+	number = document.getElementById('5');
+	number.innerHTML = 'Numero inverso: '+res;	
+}
 
+function verProblema(){
+	var ptsAlice, totAlice=0; 
+	var ptsBob, totBob=0;
+	for (var i = 0; i <3; i++) {
+		ptsAlice = document.getElementById('a'+i).value; 
+		ptsBob = document.getElementById('b'+i).value; 
+		if (ptsBob > ptsAlice) {
+			totBob = totBob + 1;
+		}else if(ptsBob < ptsAlice){
+			totAlice = totAlice + 1;
+		}
+	}
+	console.log('pts bob: '+ totBob + 'ptsAlice: ' + totAlice);
+	ptsBob = document.getElementById('bob');
+	ptsBob.innerHTML = totBob;
+	ptsAlice = document.getElementById('alice');
+	ptsAlice.innerHTML = totAlice;
 }
 
 /*
