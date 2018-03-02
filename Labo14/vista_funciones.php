1ee03d7741a4982244df6bbadfd043f39313f20a
AngieGE <30639215+AngieGE@users.noreply.github.com>
2018-03-02 08:51:18 -0600

    Lab14 terminado

--- Labo14/vista_funciones.php
+++ Labo14/vista_funciones.php
@@ -5,37 +5,18 @@
 <?php
 session_start();
   $fruit_name = isset($_POST["fruta"]) && ($_POST["fruta"]);
-  $_SESSION['fruta'] = $_POST["fruta"];
+  $R1 = isset($_POST["customRadioInline1"]);
+  $R2 = isset($_POST["customRadioInline2"]);
 
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    require_once "util.php";
+    $_SESSION['fruta'] = $_POST["fruta"];
+    echo 'R1: '.$R1.'<br>';
+    echo 'R2: '.$R2.'<br>';
 
-     require_once "util.php";
-     $result = getFruitsByName($_SESSION['fruta']);
-     //echo 'paso 1 <br>';
-     //echo 'fruta: '.$fruit_name;
+    echo getFruits($_SESSION['fruta']);
+    //echo getCheapOrExpensiveFruits("15");
 
-    if ( mysqli_num_rows($result) > 0) {
-      //Imprimo la info
-      echo ' <br> <br> <br>
-      <h1>Table Fruits</h1>
-      <table border="1" cellspacing=“4” cellpadding=“2” width="300px">
-        <tr>
-          <th>Name</th>
-          <th>Units</th>
-          <th>Quantity</th>
-          <th>Price</th>
-          <th>Country</th>
-        </tr>';
-      while ($row = mysqli_fetch_assoc($result)) {
-        echo '
-          <tr border="1" cellspacing=“4” cellpadding=“2” width="300px">
-            <td>'. $row["name"].'</td>
-            <td>'. $row["units"].'</td>
-            <td>'. $row["quantity"].'</td>
-            <td>'. $row["price"].'</td>
-            <td>'. $row["country"].'</td>
-          </tr>';
-      }
-    }
+   //$result = getFruits($_SESSION['fruta']);
   }
  ?>
