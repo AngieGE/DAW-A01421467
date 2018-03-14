eef947846b08888b43698d18192a34d03c5b0b74
AngieGE <30639215+AngieGE@users.noreply.github.com>
2018-03-13 23:36:38 -0600

    Lab 19

--- Lab19/ajax2.js
+++ Lab19/ajax2.js
@@ -1,59 +1,28 @@
+function showUser(str) {
+  console.log("ENTRO JS");
 
-function getRequestObject() {
-  // Asynchronous objec created, handles browser DOM differences
+  if (str=="") {
+    document.getElementById("txtHint").innerHTML="No hay datos";
+    return;
+ }
 
-  if(window.XMLHttpRequest) {
-    // Mozilla, Opera, Safari, Chrome IE 7+
-    return (new XMLHttpRequest());
-  }
-  else if (window.ActiveXObject) {
-    // IE 6-
-    return (new ActiveXObject("Microsoft.XMLHTTP"));
-  } else {
-    // Non AJAX browsers
-    return(null);
-  }
+ //Hago la una instanca de la conexión
+ if (window.XMLHttpRequest) {
+  // code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp = new XMLHttpRequest();
+} else {
+  // code for IE6, IE5
+  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
 
-
-function showUser(str) {
-  console.log("ENTRO AL JS");
-//  if (str=="") {
-//    document.getElementById("txtHint").innerHTML="";
-//    return;
-//  }
-  if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-  } else { // code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
+xmlhttp.open("GET","getuser.php?q="+str,true);
+xmlhttp.send();
   xmlhttp.onreadystatechange=function() {
     if (this.readyState==4 && this.status==200) {
       document.getElementById("txtHint").innerHTML = this.responseText;
     }
   }
-  xmlhttp.open("GET","getuser.php?q="+str,true);
-  xmlhttp.send();
+ 
 }
 
-function sendRequest(){
-console.log("ENTRO AL JS");
-   request=getRequestObject();
-   if(request!=null)
-   {
-     var userInput = document.getElementById('userInput');
-     var url='getuser.php?pattern='+userInput.value;
-     request.open('GET',url,true);
-     request.onreadystatechange =
-            function() {
-                if((request.readyState==4)){
-                    // Asynchronous response has arrived
-                    var ajaxResponse=document.getElementById('ajaxResponse');
-                    ajaxResponse.innerHTML=request.responseText;
-                    ajaxResponse.style.visibility="visible";
-                }
-            };
-     request.send(null);
-   }
-}
+
--- Lab19/getuser.php
+++ Lab19/getuser.php
@@ -1,80 +1,51 @@
+
 <?php
-echo "ENTRO A GETUSER";
 $q = intval($_GET['q']);
 
 $con = mysqli_connect("localhost","root","","jambe");
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }else{
-  //  mysqli_select_db($con,"ajax_demo");
-    $sql="SELECT * FROM gradoestudios WHERE idGrado = '".$q."'";
+//  mysqli_select_db($con,"ajax_demo");
+//$sql="SELECT * FROM gradoestudios WHERE idGrado = 3"; 
+$sql="SELECT * FROM gradoestudios WHERE idGrado = '".$q."'";
+  
+$result = mysqli_query($con,$sql);
 
+$row = mysqli_fetch_array($result);
+echo $q;
 
-    $result = mysqli_query($con,$sql);
-    $row = mysqli_fetch_array($result);
+echo "<br><br>l<br>";
+                echo "<div class='row'><div class='col-12'><table class='table table-hover'>";
+                echo buildTableData(queryVisitor($q,$con));
+                echo "</table></div></div>";
 
-    if(count($_POST)>0
-    && (($_POST["stat"]["user_grade"] == null) || test($NUMBER, $_POST["stat"]["user_grade"]))
-    && (($_POST["stat"]["age"] == null) || test($NAME, $_POST["stat"]["age"]))
-    && (($_POST["stat"]["sex"] == null) || test($NAME, $_POST["stat"]["sex"]))
-    && (($_POST["stat"]["suburb"] == null) || test($NAME, $_POST["stat"]["suburb"]))
-    && (($_POST["stat"]["month"] == null) || test($DATE, $_POST["stat"]["month"]))
-    && (($_POST["stat"]["year"] == null) || test($GENDER, $_POST["stat"]["year"]))
-    && (isset($_POST["actionTypeEntry"])) && (($_POST["actionTypeEntry"] == "Registrar Entrada"))
+/*
+  echo "<table>
+  <tr>
+  <th>idGrado</th>
+  <th>Nombre</th>
+  </tr>";
+  while($row = mysqli_fetch_array($result)) {
+      echo "<tr>";
+      echo "<td>" . $row['idGrado'] . "</td>";
+      echo "<td>" . $row['nombre'] . "</td>";
+      echo "</tr>";
+  }
+  echo "</table>";*/
 
-){
-    $nulls = 0;
-    foreach($_POST["stat"] as $key => $value){
-              if($value != null)
-                  $info[$key] = htmlspecialchars($value);
-              else{
-                  $info[$key] = "";
-                  if($key!="number")
-                      $nulls++;
-              }
-      }
-    if ($_POST["actionTypeEntry"]) {
-      echo "<br><br><br>";
-                    echo "<div class='row'><div class='col-12'><table class='table table-hover'>";
-                    echo buildTableData(queryVisitor(
-                                            $info["user_grade"],
-                                            $info["age"],
-                                            $info["sex"],
-                                            $info["suburb"],
-                                            $info["month"],
-                                            $info["year"],
-                                            $con
-                                            ));
-                    echo "</table></div></div>";
-                    echo '$row["nombre"]'.$row["nombre"];
-    }
-
-  /*
-    echo "<table>
-    <tr>
-    <th>idGrado</th>
-    <th>Nombre</th>
-    </tr>";
-    while($row = mysqli_fetch_array($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['idGrado'] . "</td>";
-        echo "<td>" . $row['nombre'] . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-  */
-
-    mysqli_close($con);
-}
+  
+  mysqli_close($con);
 }
 
 function buildTableData($result){
+    echo "Table";
     $table = "";
     if(mysqli_num_rows($result)>0){
         $fieldNumber = mysqli_num_fields($result);
         $table .= "<thead>";
         for($i = 0; $i < $fieldNumber; $i++){
-            $table .= "<td><strong>  ".mysqli_fetch_field_direct($result, $i)->name." </strong> </td>";
+            $table .= "<td> <strong>".mysqli_fetch_field_direct($result, $i)->name."</strong></td>";
         }
         $table .= "</thead><tbody>";
         while($row = mysqli_fetch_assoc($result)){
@@ -90,17 +61,21 @@ function buildTableData($result){
     }
     return $table;
 }
+    function queryVisitor($idGrado){
+        $con = mysqli_connect("localhost","root","","jambe");
+        $statement = mysqli_prepare($con,"
+        SELECT  g.idGrado, g.nombre as 'Grado', v.nombre, v.apellidoPaterno, v.apellidoMaterno, v.genero, 
+        FROM gradoestudios g, visitante v, visitante_gradoestudios vg
+        WHERE vg.idVisitante = v.idVisitante AND vg.idGrado = g.idGrado
+        AND (vg.idGrado = ? ".($idGrado==""?"or 1":"").")
+        ");
+        echo $idGrado;
+        $statement->bind_param("i", $idGrado);
+        $statement->execute();
+        echo $idGrado;
+        $result = $statement->get_result();
+        disconnect($con);
+        return $result;
+    }
 
-
-function queryVisitor($idGrado, $age, $sex, $suburb, $month, $year, $con){
-    $statement = mysqli_prepare($con,"
-    select ge.idGrado, ge.nombre, v.nombre, v.apellidoPaterno, v.apellidoMaterno, v.genero
-    from gradoestudios ge, visitante v
-    where (ge.idGrado = ? ".($idGrado==""?"or 1":"").")
-    ");
-    $statement->bind_param("i", $idGrado);
-    $statement->execute();
-    $result = $statement->get_result();
-  //  disconnect($connection);
-    return $result;
-}
+?>
